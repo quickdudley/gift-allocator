3932a77c580def41b1b70a937114248005c85477
Jeremy List <quick.dudley@gmail.com>
2018-01-22 16:34:38 +1300

    Can add names to the list: nothing else yet.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,78 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class Participant extends Component {
   render() {
     return (
+      <div classname="Participant">{this.props.object.name}</div>
+    );
+  }
+}
+
+class ListEdit extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: ""
+     };
+  }
+
+  changeName(e) {
+    this.setState({name: e.target.value});
+  }
+
+  doAdd(e) {
+    this.props.root.addParticipant({name: this.state.name});
+    this.setState({name: ""});
+  }
+  
+  render() {
+    var btns = this.props.editing ? [
+      {title: "Cancel", action: () => {}},
+      {title: "Save", action: () => {}}
+     ] : [
+      {title: "Add", action: () => {
+        this.doAdd();
+       }},
+      {title: "Clear", action: () => {
+        this.setState({name: ""});
+       }}
+     ];
+    btns = btns.map(btn => {
+      return (<button onClick={btn.action}>{btn.title}</button>)
+     });
+    return (<div className="ListEdit">
+      <input type="text" value={this.state.name}
+        onChange={e => {this.changeName(e)}}/>
+      <div>{btns}</div></div>);
+  }
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      participants: [],
+      number: 0
+     }
+  }
+
+  addParticipant(participant) {
+    var sn = this.state.number;
+    participant.number = sn;
+    this.state.participants.push(participant);
+    this.setState({number: sn + 1});
+  }
+  
+  render() {
+    var participants = [];
+    for (let p of this.state.participants) {
+      participants.push(<Participant object={p} key={p.number} />);
+     }
+    return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       <div className="participantList">{participants}
+       <ListEdit root={this}/></div>
       </div>
     );
   }
